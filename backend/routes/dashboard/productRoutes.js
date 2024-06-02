5474e14e4f2593a15247ced72814516a92517167
pankaj suthar <pankajss0070@gmail.com>
2024-06-02 21:45:23 +0530

    add product

---
+++ backend/routes/dashboard/productRoutes.js
@@ -0,0 +1,7 @@
+const productController = require("../../controllers/dashboard/productController");
+const { authMidleware } = require("../../middleware/authMiddleware");
+const router = require("express").Router();
+
+router.post("/product-add", authMidleware, productController.add_product);
+
+module.exports = router;

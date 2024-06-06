9568853ac82185557b02c2b928d5055e1b1cb05c
pankaj suthar <pankajss0070@gmail.com>
2024-06-06 18:20:42 +0530

    cart page design

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -205,7 +205,9 @@ const Header = () => {
 
                     <div className="relative flex justify-center items-center cursor-pointer w-[35px] h-[35px] rounded-full bg-[#e2e2e2]">
                       <span className="text-xl text-green-500">
-                        <FaCartShopping />
+                        <Link to={"/cart"}>
+                          <FaCartShopping />
+                        </Link>
                       </span>
                       <div className="w-[20px] h-[20px] absolute bg-red-500 rounded-full text-white flex justify-center items-center -top-[3px] -right-[5px] ">
                         {wishlist_count}

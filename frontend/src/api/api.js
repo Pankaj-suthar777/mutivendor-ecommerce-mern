8c6fdcb7a7c2eac02fc18d128c79e612f3352630
pankaj suthar <pankajss0070@gmail.com>
2024-07-01 14:43:07 +0530

    some fixes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-const local = "http://localhost:5000";
-//const production = "";
+//const local = "http://localhost:5000";
+const production = "https://mutivendor-ecommerce-mern-api.vercel.app";
 const api = axios.create({
-  baseURL: `${local}/api`,
+  baseURL: `${production}/api`,
 });
 
 export default api;

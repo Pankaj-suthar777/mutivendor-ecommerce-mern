adac7ab66898474c0a8913323940c3c0d0b4fd6c
pankaj suthar <pankajss0070@gmail.com>
2024-07-01 15:04:35 +0530

    server deploy fixed

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 //const local = "http://localhost:5000";
-const production =
-  "https://mutivendor-ecommerce-mern-apuy0ihzk-pankajs-projects-1f54d63d.vercel.app";
+const production = "https://mutivendor-ecommerce-mern-api.vercel.app";
 const api = axios.create({
   baseURL: `${production}/api`,
 });

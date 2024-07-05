c2010b345913390d6c886eb142ad8ad1fd64738d
pankaj suthar <146710838+Pankaj-suthar777@users.noreply.github.com>
2024-07-05 14:43:36 +0530

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -3,6 +3,10 @@ const jwt = require("jsonwebtoken");
 module.exports.authMidleware = async (req, res, next) => {
   const { accessToken } = req.cookies;
 
+  console.log("aaaccc",accessToken);
+    console.log("req.cookies",req.cookies);
+
+
   if (!accessToken) {
     return res.status(409).json({ error: "Please Login First" });
   } else {
